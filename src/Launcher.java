9a1c37d339cf7fdd46e6b0ff044b9686c79e8431
bagussya <74695094+bagussya@users.noreply.github.com>
2020-12-08 22:39:48 +0700

    Update Launcher.java

--- src/Launcher.java
+++ src/Launcher.java
@@ -6,15 +6,20 @@ import javax.swing.SwingUtilities;
 public class Launcher {
 
 	public static void main(final String[] args) {
-	    SwingUtilities.invokeLater(() -> {
-	        final JFrame frame = new JFrame();
-	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	        frame.setTitle("Sliding Puzzle");
-	        frame.setResizable(false);
-	        frame.add(new Main(4, 550, 30), BorderLayout.CENTER);
-	      	frame.pack();
-	      	frame.setLocationRelativeTo(null);
-	      	frame.setVisible(true);
-	    });
+		Scanner sc = new Scanner (System.in);
+		int n;
+		System.out.print("Puzzle n X n, n =");
+        	n=Integer.parseInt(sc.nextLine());
+		
+	    	SwingUtilities.invokeLater(() -> {
+	        	final JFrame frame = new JFrame();
+	        	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	        	frame.setTitle("Sliding Puzzle");
+	        	frame.setResizable(false);
+	        	frame.add(new Main(4, 550, 30), BorderLayout.CENTER);
+	      		frame.pack();
+	      		frame.setLocationRelativeTo(null);
+	      		frame.setVisible(true);
+	    	});
 	}   
 }
